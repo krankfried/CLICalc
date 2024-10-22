9d89943b8d08ffb34a7eebfdeaa6bd10003bcca2
krankfried <officialphai@gmail.com>
2024-10-22 03:19:39 +0200

    Small code adjustment

--- calculator.h
+++ calculator.h
@@ -6,11 +6,26 @@
 #ifndef CALCULATOR_H
 #define CALCULATOR_H
 
+#include <string>
+#include <array>
+#include <tuple>
+
 
 double calc(double r1, char op, double r2);
 
 class calculator {
+private:
+std::array<std::string, 9> const messages;
+std::tuple<double, char, double, double> calc_history;
 
+
+public:
+ [[nodiscard]] std::tuple<double, char, double, double> getHistory() const{
+     return calc_history;
+ }
+ [[nodiscard]] std::array<std::string, 9> greetUser() const {
+    return messages;
+ }
 };
 
 
